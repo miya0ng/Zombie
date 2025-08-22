338ffd0c4ae647707765d0557d322b6b0a6ade45
이승연 <leelaf0002@gmail.com>
2025-08-22 17:39:32 +0900

    gun 스크립트 작성

---
+++ Zombie/Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public ParticleSystem muzzleEffect;
+    public ParticleSystem shellEffect;
+
+    private LineRenderer lineRenderer;
+    private AudioSource audioSource;
+
+    public Transform firePosition;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 2;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(ShotEffect());
+        }
+    }
+    private IEnumerator ShotEffect()
+    {
+
+        muzzleEffect.Play();
+        shellEffect.Play();
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, firePosition.position);
+        Vector3 endPos = firePosition.position + firePosition.forward * 10f; //@
+
+        lineRenderer.SetPosition(1, endPos);
+        yield return new WaitForSeconds(1f);
+        lineRenderer.enabled = false;
+
+
+
+
+
+    }
+}
